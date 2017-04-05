c9b2afcaff41de641922fa4012bfefae38ea3bf4
Joshua Cole <joshuafcole@gmail.com>
2017-04-04 19:02:17 -0700

    Touch up the program switcher with some CSS

--- programs/program-switcher.ts
+++ programs/program-switcher.ts
@@ -4,10 +4,34 @@ let prog = new Program("test");
 prog.attach("ui");
 
 prog
+  .block("Draw a container for the program links.", ({record}) => {
+    return [
+      record("container", "ui/column").add("children", [
+        record("ui/text", {sort: 0, text: "Select a program", style: record({padding: "5px 10px"})})
+      ]),
+      record("html/element", {tagname: "style", text: `
+        body { justify-content: center; }
+        .container {flex: 0 0 auto; align-self: center; align-items: flex-start; padding: 20; background: white; border-radius: 3px; box-shadow: 0 3px 4px rgba(0, 0, 0, 0.1); }
+        .program-button {color: rgb(65, 161, 221); text-decoration: underline; cursor: pointer; }
+      `})
+    ];
+  })
   .block("Draw a button for each program.", ({find, record}) => {
+    let container = find("container");
     let program = find("program");
     return [
-      record("program-button", "ui/button", {program, text: program.url})
+      container.add("children", [
+        record("program-button", "ui/button", {sort: program.url, program, text: program.url})
+      ])
+    ];
+  })
+  .block("If there aren't any programs, tell the user.", ({find, not, record}) => {
+    let container = find("container");
+    not(() => find("program"));
+    return [
+      container.add("children", [
+        record("ui/text", {text: "I wasn't able to find any programs in the specified workspace(s). :("})
+      ])
     ];
   });
 
