af75e1cbf9f839963731a9de274681bbc40b9cba
Joshua Cole <joshuafcole@gmail.com>
2017-04-06 20:55:08 -0700

    Add an intro to the program switcher

--- programs/program-switcher.ts
+++ programs/program-switcher.ts
@@ -20,21 +20,53 @@ prog.attach("ui");
 prog
   .bind("Draw a container for the program links.", ({record}) => {
     return [
-      record("container", "ui/column"),
+      record("ui/spacer", {sort: 1}),
+      record("intro-container", "container", "html/element", {sort: 2, tagname: "div"}),
+      record("program-container", "container", "ui/column", {sort: 3}),
+      record("ui/spacer", {sort: 4}),
       record("html/element", {tagname: "style", text: `
-        body { justify-content: center; }
-        .container {flex: 0 0 auto; align-self: center; align-items: flex-start; padding: 20; background: white; border-radius: 3px; box-shadow: 0 3px 4px rgba(0, 0, 0, 0.1); }
+        body { flex-direction: row; justify-content: space-around; align-items: center; }
+        a { display: inline-block; }
+        .container {flex: 0 0 auto; align-items: flex-start; max-width: 500; margin: 20; padding: 20; background: white; border-radius: 3px; box-shadow: 0 3px 4px rgba(0, 0, 0, 0.1); line-height: 1.5; }
+
+        .intro-container { flex: 0 1 auto; }
+        .container > text { display: inline; }
+        .container > .header { display: block; width: 100%; margin-bottom: 1em; text-align: center; }
+        .container > .code { padding: 0 5; background: #eee; border-radius: 3px; white-space: nowrap; }
+
         .program-button {color: rgb(65, 161, 221); cursor: pointer; }
         .program-button:hover {color: rgb(78, 193, 255); text-decoration: underline; }
+
+        @media (max-width: 800px) {
+          body { flex-direction: column; align-items: stretch; }
+          .container { max-width: initial; }
+        }
       `})
     ];
   })
+  .bind("Fill the intro box with some friendly introductory text.", ({find, record}) => {
+    let container = find("intro-container");
+    return [
+      container.add("children", [
+        record("ui/text", {sort: 1, class: "header", text: "Eve Starter"}),
+        record("ui/text", {sort: 2, text: "The starter is a playground for exploring examples and building new applications with Eve 0.3.0.\n\nClick on a link below to execute a program. Follow along with its source code in your editor of choice in "}),
+        record("ui/text", {sort: 3, class: "code", text: "eve-starter/programs/"}),
+        record("ui/text", {sort: 4, text: ". For a primer on working with Eve's new JavaScript DSL, check out "}),
+        record("html/element", {sort: 5, tagname: "a", href: "https://github.com/witheve/docs/blob/master/guides/dsl.md", text: "the DSL guide"}),
+        record("ui/text", {sort: 6, text: ".\n\nOnce you've explored the examples and want to start writing your own programs, you can modify the starter to suit your needs.  For more advanced functionality of the starter, run "}),
+        record("ui/text", {sort: 7, class: "code", text: "npm start -- --help"}),
+        record("ui/text", {sort: 8, text: "."}),
+
+      ])
+    ];
+  })
+
   .bind("Draw a button for each program.", ({find, record}) => {
-    let container = find("container");
+    let container = find("program-container");
     let program = find("program");
     return [
       container.add("children", [
-        record("ui/text", {sort: 0, text: "Select a program", style: record({padding: "5px 10px"})}),
+        record("ui/text", {sort: 0, class: "header", text: "Select a program"}),
         record("program-button", "ui/button", {sort: program.url, program, text: program.url})
       ])
     ];
